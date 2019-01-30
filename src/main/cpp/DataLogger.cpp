0c652001ce1ffb5fd5111edfe6e8e3c369ff068c
Jacob Krucinski <jacob1576@gmail.com>
2019-01-29 19:48:05 -0500

    Incomplete Datalogger code

---
+++ src/main/cpp/DataLogger.cpp
@@ -0,0 +1,15 @@
+// This program will act as a data logger for rpm
+#include "DataLogger.h"
+/**
+void DataLogger::addRPM(double rpmToAdd) {
+    rpm.push_back(rpmToAdd);
+}
+
+void DataLogger::addTimeStamp(long timestampToAdd){
+    timestamp.push_back(rpmToAdd);
+}
+
+void DataLogger::writeToTXTFile(string filename){
+
+}
+**/
---
+++ src/main/include/DataLogger.h
@@ -0,0 +1,17 @@
+// This program will act as a data logger for rpm
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+/**
+class Datalogger {
+public:
+    vector<double> rpm;
+    vector<long> timestamp;
+
+    void addRPM(double rpmToAdd);
+    void addTimeStamp(long timestampToAdd);
+    void writeToTXTFile(string filename); 
+}
+**/
