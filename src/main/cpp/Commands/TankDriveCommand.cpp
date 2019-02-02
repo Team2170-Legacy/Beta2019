e231a7a9d90e4a76b2acfe5c9f15106fe5b3540c
Jacob Krucinski <jacob1576@gmail.com>
2019-02-02 14:38:42 -0500

    Small mistake fixed in switch statement

--- src/main/cpp/Commands/TankDriveCommand.cpp
+++ src/main/cpp/Commands/TankDriveCommand.cpp
@@ -70,7 +70,7 @@ void TankDriveCommand::Execute() {
             break;
         case arcadeDriveVelocity:
             leftMove = Robot::oi->getJoystickLeft()->GetY(frc::GenericHID::JoystickHand::kLeftHand);
-            leftRotate = Robot::oi->getJoystickRight()->GetX(frc::GenericHID::JoystickHand::kRightHand);
+            leftRotate = Robot::oi->getJoystickRight()->GetX(frc::GenericHID::JoystickHand::kLeftHand);
             leftMove = -leftMove;
             Robot::driveTrain->ArcadeDriveVelocity(leftRotate, leftMove, false);
             break;
